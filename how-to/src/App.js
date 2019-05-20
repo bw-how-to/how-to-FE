194b6affb8b477abb6195fd343da890ac99c9acf
josh182014 <josh182014@gmail.com>
2019-05-20 08:43:30 -0600

    added sign up form and login form (not functional yet)

--- how-to/src/App.js
+++ how-to/src/App.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+import SignUp from './components/SignUp'
+import Login from './components/Login'
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h3>How To</h3>
-    </div>
+    <Router>
+      <div className="App">
+        <h3>How To</h3>
+        <div>
+          <Link to="/signup">Sign Up</Link>
+          <Link to="/login">Login</Link>
+        </div>
+        <Route path='/signup' component={SignUp}/>
+        <Route path='/login' component={Login}/>
+      </div>
+    </Router>
   );
 }
 
---
+++ how-to/src/components/Login.js
@@ -0,0 +1,50 @@
+import React from 'react'
+
+export default class Login extends React.Component {
+    state = {
+        credentials: {
+            username: '',
+            password: '',
+            confirmPassword: ''
+        }
+    }
+
+    handleChange = (e) => {
+        this.setState({
+            credentials: {
+                ...this.state.credentials,
+                [e.target.name]: e.target.value
+            }
+        })
+    }
+
+    render() {
+        return (
+            <div className='signUpForm'>
+                <form onSubmit={this.login}>
+                    <input
+                        type='text'
+                        name='username'
+                        placeholder='Username'
+                        autoComplete='off'
+                        onChange={this.handleChanges}
+                        value={this.state.username}
+                        required
+                    />
+                    <input
+                        type='password'
+                        name='password'
+                        placeholder='Password'
+                        autoComplete='off'
+                        onChange={this.handleChanges}
+                        value={this.state.password}
+                        required
+                    />
+                    <button>
+                        Login
+                    </button>
+                </form>
+            </div>
+        )
+    }
+}
